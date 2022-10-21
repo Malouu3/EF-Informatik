9c5603260c8b127c3c5f373fd3b6a98efa4c17cc
Malou <marie-louise.zesiger@edu.gbsl.ch>
2022-10-21 15:42:58 +0200

    m

--- exercises/primzahlen.py
+++ exercises/primzahlen.py
@@ -2,13 +2,21 @@
 
 primzahlen = []
 
-for nummer in range(1, 1001):           # läuft alle Zahlen von 1 - 101 ab und setzt sie in die Variable "nummer"
-    if nummer > 1:                     # Wenn "nummer" grösser als 1 ist:
-        for i in range(2, nummer):      # dann wird die nummer durch die variable "i" geteilt, die von null bis
-            if (nummer % i) == 0:       # zum Wert "nummer" alle einmal darstellt. Wenn jetzt aus der Division
-                break                   # keinen Restwert entsteht, wird der vorgang für den derzeitigen Wert
-                                        # "nummer" abgebrochen. Wenn es keinen Rest gibt, wird "nummer" ausgegeben.
+for nummer in range(1, 1001):           
+    if nummer > 1:                     
+        for i in range(2, nummer):      
+            if (nummer % i) == 0:       
+                break                   
+                                        
         else:
-            primzahlen.append(nummer)   #Setzt die Zahl in die Liste Primzahlen
+            primzahlen.append(nummer)   
 
 print(primzahlen)
+
+# läuft alle Zahlen von 1 - 101 ab und setzt sie in die Variable "nummer"
+# Wenn "nummer" grösser als 1 ist:
+# dann wird die nummer durch die variable "i" geteilt, die von null bis
+# zum Wert "nummer" alle einmal darstellt. Wenn jetzt aus der Division
+# keinen Restwert entsteht, wird der vorgang für den derzeitigen Wert
+# "nummer" abgebrochen. Wenn es keinen Rest gibt, wird "nummer" ausgegeben.
+#Setzt die Zahl in die Liste Primzahlen
