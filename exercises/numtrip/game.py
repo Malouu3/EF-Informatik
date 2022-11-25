26e2fe19e03ed89893e263be51a865e14dd4092d
Malou <marie-louise.zesiger@edu.gbsl.ch>
2022-11-25 15:47:14 +0100

    zeilennummer hinzugefügt

--- exercises/numtrip/game.py
+++ exercises/numtrip/game.py
@@ -11,29 +11,32 @@ board = [
 def Brett():
 # Zeilen nacheinander aus dem Board
     spalten = len(board[1])+1
-    print('   ', end='')
+    print('    ', end='')
 
     for spalte in range(1, spalten):
-        print(f' {spalte}', end='')
+        print(f'  {spalte} ', end='')
 
     print('')
     for zeile in board:
+        zeilennummer = board.index(zeile) + 1
+
         print('   ', end='')
         for zelle in zeile:
             # macht die wagrechten linien. so viele Linien wie anzahl Zellen
-            print('--', end='')
+            print(' -- ', end='')
         print('-')
-        print(zeile, ' ', end='')
+        
+        print(zeilennummer, ' ', end='')
         #Bildet die einzelnen Werte der Zellen nacheinader ab.
         for zelle in zeile:
             #f'' formatiert Zellenwert mit Text. end='' verhindert den Zeilenumbrung
-            print(f'|{zelle}', end='')
+            print(f'| {zelle} ', end='')
         print('|')
 
     # die untersten wagrechten Linien
     print('   ', end='')
     for zelle in board[0]:
-        print('--', end='')
+        print(' -- ', end='')
     print('-')
 
 Brett()
