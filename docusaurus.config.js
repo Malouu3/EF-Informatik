f6b79ed4b5fec631b7a4d67d8911df4419f9e83b
Malouu3 <marie-louise.zesiger@edu.gbsl.ch>
2022-12-07 16:29:35 +0100

    Anpassung docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,8 +3,8 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const USERNAME = 'foobar';
-const REPO_NAME = 'repo';
+const USERNAME = 'Malouu3';
+const REPO_NAME = 'EF-Informatik';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
